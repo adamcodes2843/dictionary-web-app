3f1d6caf2b4c049a29d4a9eb08d4e12062cc2cc5
Adam Childers <adamcodes2843@gmail.com>
2023-04-23 22:04:58 -0500

    feat: dark mode

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import {useEffect,useState} from 'react'
+import {useEffect,useState, createContext} from 'react'
 import Nav from './components/Nav'
 import WordPhonetic from './components/WordPhonetic'
 import SearchBar from './components/SearchBar'
@@ -8,11 +8,13 @@ import Meanings from './components/Meanings'
 import Footer from './components/Footer'
 import NoDefinition from './components/NoDefinition'
 
+export const ThemeContext = createContext(null)
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home({}) {
 
+  const [theme, setTheme] = useState<any>('light')
   const [word, setWord] = useState<string>('keyboard')
   const [data, setData] = useState<any | null>(null)
   const [darkMode, setDarkMode] = useState<boolean>(false)
@@ -22,8 +24,7 @@ export default function Home({}) {
 
 
   console.log(data)
-  console.log(word)
-  console.log(textInput)
+  console.log(theme)
 
 useEffect(() => {
   fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
@@ -40,15 +41,21 @@ const handleKeyDown = (e: any) => {
     setWord(textInput)
   }
 }
+
+const toggleTheme = () => {
+  setTheme((curr:string) => (curr === 'light' ? 'dark' : 'light'))
+}
   
   return (
-    <main className={`flex flex-col items-center justify-between gap-3 font-${font}`}>
-        <Nav setDarkMode={setDarkMode} setFont={setFont} font={font} darkMode={darkMode} popUp={popUp} setPopUp={setPopUp} />
-        <SearchBar handleChange={handleChange} handleKeyDown={handleKeyDown} textInput={textInput} setWord={setWord} word={word} />
+    <ThemeContext.Provider value={{theme, toggleTheme}}>
+    <main className={`${theme === 'dark' ? 'bg-grayscale-800 text-grayscale-100' : ''} p-8 flex flex-col items-center justify-between gap-3 font-${font} h-min-screen`}>
+        <Nav setDarkMode={setDarkMode} setFont={setFont} font={font} darkMode={darkMode} popUp={popUp} setPopUp={setPopUp} toggleTheme={toggleTheme} theme={theme} />
+        <SearchBar handleChange={handleChange} handleKeyDown={handleKeyDown} textInput={textInput} setWord={setWord} word={word} theme={theme} />
         {data && <WordPhonetic data={data} />}
         {data ? <Meanings data={data} setWord={setWord} setTextInput={setTextInput} />
         : !data && word === '' ? "suh" : <NoDefinition />}
         {data && <Footer data={data} />}
     </main>
+    </ThemeContext.Provider>
   )
 }
