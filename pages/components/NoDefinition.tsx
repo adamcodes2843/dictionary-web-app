9909f9f684fb8c1d41151b9b78a7996764374964
Adam Childers <adamcodes2843@gmail.com>
2023-06-07 21:29:37 -0500

    fixed typo

--- pages/components/NoDefinition.tsx
+++ pages/components/NoDefinition.tsx
@@ -6,7 +6,7 @@ const NoDefinition = () => {
     <div className="w-full  flex flex-col items-center mt-28 mb-auto">
         <Image src={face} alt="confused emoji" className="mb-6 h-16 w-16" />
         <h2 className="font-bold mb-6">No Definitions Found</h2>
-        <p className='text-center text-grayscale-400'>Sorry pal, we couldn't find definitions for the word you were looking for. You can try the search again at a lter time or head to the web instead.</p>
+        <p className='text-center text-grayscale-400'>Sorry pal, we couldn't find definitions for the word you were looking for. You can try the search again at a later time or head to the web instead.</p>
     </div>
   )
 }
